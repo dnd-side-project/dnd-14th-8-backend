9a6ce5a771c42244568fd6d5269dc9cab5f1bcb2
BaekDoHyeon <qorehgus0823@naver.com>
2026-01-26 21:32:13 +0900

    feat(exception) : MethodArgumentNotValidException 예외처리 추가

--- src/main/java/com/dnd/moyeolak/global/exception/GlobalExceptionAdvice.java
+++ src/main/java/com/dnd/moyeolak/global/exception/GlobalExceptionAdvice.java
@@ -5,9 +5,14 @@ import com.dnd.moyeolak.global.response.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE) // 다른 예외 처리기보다 우선순위 높게 설정
@@ -17,10 +22,26 @@ public class GlobalExceptionAdvice {
     /** 비즈니스 예외 처리 */
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ApiResponse<Void>> handleBusinessException(BusinessException ex) {
-        log.warn("BusinessException 발생: {}", ex.getMessage());
+        log.warn("[{}] BusinessException 발생: {}", ex.getErrorCode() ,ex.getMessage());
         return ResponseEntity
                 .status(ex.getErrorCode().getHttpStatus())
                 .body(ApiResponse.error(ex.getErrorCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        log.warn("Validation 실패: {}", errors);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(ErrorCode.INVALID_PARAMETER, errors));
     }
 
     @ExceptionHandler(Exception.class)
