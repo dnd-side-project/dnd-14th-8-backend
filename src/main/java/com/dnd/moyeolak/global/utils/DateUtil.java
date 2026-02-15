f211df00eae38baec184b08bd0899ed02604fb18
BaekDoHyeon <qorehgus0823@naver.com>
2026-02-15 15:08:01 +0900

    feat (utils) : DateUtil 유틸 클래스 생성

---
+++ src/main/java/com/dnd/moyeolak/global/utils/DateUtil.java
@@ -0,0 +1,30 @@
+package com.dnd.moyeolak.global.utils;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+public class DateUtil {
+
+    private static final Map<DayOfWeek, String> DAY_OF_WEEK_KOREAN = Map.of(
+            DayOfWeek.MONDAY, "월요일",
+            DayOfWeek.TUESDAY, "화요일",
+            DayOfWeek.WEDNESDAY, "수요일",
+            DayOfWeek.THURSDAY, "목요일",
+            DayOfWeek.FRIDAY, "금요일",
+            DayOfWeek.SATURDAY, "토요일",
+            DayOfWeek.SUNDAY, "일요일"
+    );
+
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+    public static String toDayOfWeek(LocalDateTime dateTime) {
+        return DAY_OF_WEEK_KOREAN.get(dateTime.getDayOfWeek());
+    }
+
+    public static String formatTime(LocalDateTime dateTime) {
+        return dateTime.format(TIME_FORMATTER);
+    }
+
+}
