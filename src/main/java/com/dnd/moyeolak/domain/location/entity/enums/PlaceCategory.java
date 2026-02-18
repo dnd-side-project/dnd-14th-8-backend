a9055c738dc03030f2f21700d2d341e195238822
BaekDoHyeon <qorehgus0823@naver.com>
2026-02-19 00:08:52 +0900

    feat (entity) : 주변 장소 검색 엔티티 정의 (NearbyPlace, NearbyPlaceHours, PlaceCategory)

---
+++ src/main/java/com/dnd/moyeolak/domain/location/entity/enums/PlaceCategory.java
@@ -0,0 +1,8 @@
+package com.dnd.moyeolak.domain.location.entity.enums;
+
+public enum PlaceCategory {
+    STUDY_CAFE,     // 스터디카페
+    MEETING_ROOM,   // 회의실
+    CAFE,           // 카페
+    RESTAURANT      // 음식점
+}
